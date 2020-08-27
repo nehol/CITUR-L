82e71959bf2ef9fe200814d8db7e3ddd1224fe82
Elisha Hollander <just4now666666@gmail.com>
2020-08-28 01:22:01 +0300

    Create python-rosehip.mk

---
+++ build/external/package/python-rosehip/python-rosehip.mk
@@ -0,0 +1,5 @@
+PYTHON_ROSEHIP_SOURCE = rosehip-1.0.2.tar.gz
+PYTHON_ROSEHIP_SITE = https://files.pythonhosted.org/packages/6d/a0/44a9527ede18c61765452497e17090b3dfb586589292b6361d1f26753e9f
+PYTHON_ROSEHIP_SETUP_TYPE = setuptools
+PYTHON_ROSEHIP_LICENSE = MIT
+$(eval $(python-package))
